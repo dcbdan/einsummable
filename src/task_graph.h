28947d1d7a42375326d6a83e4265c36eae50fc6b
Daniel Bourgeois <dcb_dan@hotmail.com>
2023-04-08 21:58:11 -0500

    Started task graph; wrote partialize comment

---
+++ src/task_graph.h
@@ -0,0 +1,99 @@
+#pragma once
+#include "setup.h"
+
+#include "einsummable.h"
+#include "graph.h"
+
+struct taskgraph_t {
+  // Methods to construct a task graph object
+  // {{{
+  int insert_input(
+    int loc,
+    vector<uint64_t> shape);
+  int insert_einsummable(
+    int loc,
+    einsummable_t e,
+    vector<int> inns);
+  int insert_move(
+    int src,
+    int dst,
+    int tid);
+  // TODO: what does the partialize interface look like?
+
+
+  // }}}
+
+private:
+  struct input_t {
+    vector<uint64_t> shape;
+  };
+  struct compute_t {
+    std::variant<input_t, einsummable_t>;
+  };
+
+  // Some words are neccessary to describe what a partialize is.
+  // A partialize operation produces a single tid.
+  // Partialize represent a generalization of "multiple" partial reduces.
+  // Partialize operations may have concurrent
+  // write operations and may have multiple order-agnostic increments.
+  //
+  // Consider A = (B1; B2) + (C1; C2) + D + E
+  // Here, A D and E are contiguous, but B and C are split in the same way.
+  // And we want to compute
+  //   A1 = B1 + C1 + D1 + E1
+  //   A2 = B2 + C2 + D2 + E2
+  // This is represented as a partialize:
+  // 1. Have two separate concurrent writes, one for A1 and another for A2.
+  //    When both complete, the tid for A is available.
+  // 2. Each A1 and A2 consists of an aggregation of some inputs. Take A1:
+  //    B1,C1,D and E may be available at different times, so write into A1
+  //    one at a time and order is not important. On the first call, initialize.
+  //    So if D is avilable first, copy the corresponding range of D1 into A1 and
+  //    for each input therafter, sum into the output.
+  // Note that here, we are not forming A1, D1 or E1, but instead only a portion of
+  // A, D and E are being used at a time. These are specified with an
+  // offset for each dimenison. Consider
+  //   struct touchdim_t {
+  //     uint64_t d_inn;
+  //     uint64_t d_out;
+  //     uint64_t offset_inn;
+  //     uint64_t offset_out;
+  //     uint64_t size;
+  //   };
+  // and vectors x,y with x[:u] := y[v:v+u]. That has
+  //   d_inn = |y|
+  //   d_out = |x]
+  //   offset_inn = 0
+  //   offset_out = v
+  //   size = u
+  // For arbitrary ranked tensors, life gets more complicated. In this case,
+  // a offset and size have to be specified for each dimension and the product
+  // over the dimensions specifies the given regions.
+  //
+  // A partial_unit is the Ax = Bx + Cx + Dx + Ex
+  // and a partialize is a list of partialize units.
+  // It must be the case that each partial_unit is representing
+  // a disjoint portion of the output.
+  struct partial_unit_t {
+    // Each partial unit can have a different castable.
+    // If the number of inputs is one, this value does not get used.
+    castable_t castable;
+
+    // For each dimension in the write shape,
+    //   contain the offset and the size for the write
+    vector<tuple<uint64_t, uint64_t>> write_offset_and_size;
+
+    // For each input, contain the input offset
+    vector<tuple<int, vector<uint64_t>>> inns_and_their_offsets;
+  };
+  struct partialize_t {
+    vector<uint64_t> write_shape;
+    vector<partial_unit_t> units;
+  };
+};
+
+tuple<
+  map<int, tensor_t<int> > // for each output id, the tids of the blocks
+  taskgraph_t>             // the actual taskgraph
+make_taskgraph(graph_t const& graph);
+
