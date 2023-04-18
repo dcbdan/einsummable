7ccc90da786abc316a3a9be437eb8333da45bec5
Daniel Bourgeois <dcb_dan@hotmail.com>
2023-04-18 17:03:01 -0500

    [WIP] Added coster outline
    * a coster object will be used to determine the cost of a
      graph object
    * not implemented, but pseudo code has been added

---
+++ src/coster.cc
@@ -0,0 +1,101 @@
+#include "coster.h"
+
+cluster_t cluster_t::make(
+  vector<cluster_t::device_t> const& devices,
+  vector<cluster_t::connection_t> const& cs)
+{
+  // Remove duplicate connections in cs
+  // by summing the corresponding bandwidth
+
+  vector<connection_t> connections;
+  map<tuple<int, int>, int> to_connection;
+
+  for(auto const& c: cs) {
+    auto key = c.key();
+
+    if(to_connection.count(key) > 0) {
+      connections[to_connection[key]].bandwidth += c.bandwidth;
+    } else {
+      connections.push_back(c);
+      to_connection.insert({key, connections.size()-1});
+    }
+  }
+
+  return cluster_t {
+    .devices = devices,
+    .connections = connections,
+    .to_connection = to_connection
+  };
+}
+
+float cluster_t::move(int src, int dst, uint64_t bytes) const {
+  connection_t const& c = connections[to_connection.at({src,dst})];
+  return (1.0 / c.bandwidth) * bytes;
+}
+
+float cluster_t::compute(int loc, uint64_t flops) const {
+  device_t const& d = devices[loc];
+  return (1.0 / d.compute) * flops;
+}
+
+twolayergraph_t twolayergraph_t::make(graph_t const& graph) {
+  twolayergraph_t ret(graph);
+
+  for(auto const& graph_id: graph.get_order()) {
+    // for every out block,
+    //   set up a join_t object
+
+    // collect the usage objects and the refinemet multiple partition
+
+    // set up the refinement nodes
+  }
+
+  return ret;
+}
+
+costgraph_t costgraph_t::make(twolayergraph_t const& twolayer) {
+
+  // maintain a map from (rid_t, loc) to costgraph id (as a tensor)
+  // maintain a map from jid_t to costgraph id        (as a vector)
+  // for every twolayergraph id in order,
+  //   if it is a refinement:
+  //     collect the usage locations
+  //     do the moves
+  //     save into the map
+  //   if it is a join:
+  //     get the location
+  //     get the costgraph dependencies
+  //     add a costgraph comptue ndoe
+  //     save into map
+
+  return costgraph_t();
+}
+
+//template <typename T>
+//using priority_queue_least = std::priority_queue<T, vector<T>, std::greater<T>>;
+// For priority_queue_least, the top most element is the smallest,
+// which is the opposite behaviour of priority_queue which puts the
+// largest element at the top.
+
+float costgraph_t::operator()(cluster_t const& cluster) const {
+
+  // maintain a priority queue of in progress operations
+  // maintain a worker to set of pending ops
+  // maintain available worker resources
+  //
+  // while in_progress is not empty:
+  //   pop from in progress
+  //   update the time
+  //   remove resources from the workers
+  //   add to pending
+  //
+  //   for each worker,
+  //     randomly pick something within the capacity available
+  //     and add to pending
+  //     (if no capacity, don't do anything)
+  //
+  // return the time
+
+  return 0.0;
+}
+
---
+++ src/coster.h
@@ -0,0 +1,136 @@
+#pragma once
+#include "setup.h"
+
+#include "graph.h"
+
+struct cluster_t {
+  struct device_t {
+    uint64_t compute; // flop per second per capacity
+    int capacity;     // the number of workers or streams
+  };
+
+  struct connection_t {
+    uint64_t bandwidth; // bytes per second
+    int src;
+    int dst;
+
+    tuple<int,int> key() const { return {src,dst}; }
+  };
+
+  static cluster_t make(vector<device_t> const& ds, vector<connection_t> const& cs);
+
+  connection_t const& get_connection(int src, int dst) const {
+    return connections[to_connection.at({src,dst})];
+  }
+
+  float move(int src, int dst, uint64_t bytes) const;
+
+  float compute(int loc, uint64_t flops) const;
+
+  vector<device_t> const devices;
+  vector<connection_t> const connections;
+  map<tuple<int, int>, int> const to_connection;
+};
+
+struct twolayergraph_t {
+  static twolayergraph_t make(graph_t const& graph);
+
+  struct gid_t {
+    int id;
+    int index;
+  };
+  using rid_t = int; // refinement ids
+  using jid_t = int; // join ids
+
+  struct agg_unit_t {
+    uint64_t bytes;
+    vector<jid_t> deps;
+  };
+
+  struct refinement_t {
+    vector<agg_unit_t> units;
+    vector<jid_t> outs;
+  };
+
+  struct join_t {
+    uint64_t flops;
+    gid_t gid;
+    vector<rid_t> deps;
+    vector<rid_t> outs;
+  };
+
+  int const& join_location(gid_t const& gid) const {
+    auto const& [id, index] = gid;
+    return graph.nodes[id].placement.locations.get()[index];
+  }
+
+  vector<join_t> joins;
+  vector<refinement_t> refinements;
+
+  graph_t const& graph;
+
+private:
+  twolayergraph_t(graph_t const& graph)
+    : graph(graph)
+  {}
+};
+
+struct costgraph_t {
+  static costgraph_t make(twolayergraph_t const& twolayer);
+  float operator()(cluster_t const& cluster) const;
+
+private:
+  struct compute_t {
+    int loc;
+    uint64_t flops;
+    int capacity;
+  };
+
+  struct move_t {
+    int src;
+    int dst;
+    uint64_t bytes;
+  };
+
+  struct node_t {
+    set<int> inns;
+    set<int> outs;
+    std::variant<compute_t, move_t> op;
+  };
+
+  vector<node_t> nodes;
+
+  // TODO
+  int insert_compute(int loc, uint64_t flops, int capacity, vector<int> const& deps);
+  int insert_move(int src, int dst, uint64_t bytes, vector<int> const& deps);
+  int insert(std::variant<compute_t, move_t> op, vector<int> const& deps);
+};
+
+// This object takes a graph with a fixed partition
+// and efficiently recosts it. It tracks the graph object
+// by reference.
+//
+// Example usage:
+//   graph_t graph = create graph with initial partition
+//   coster_t coster(graph, cluster);
+//   until stop:
+//     modify graph's placements
+//     current_cost = coster()
+struct coster_t {
+  coster_t(graph_t const& g, cluster_t const& cluster)
+    : cluster(cluster), twolayer(twolayergraph_t::make(g))
+  {}
+
+  float operator()() const {
+    return this->operator()(cluster);
+  }
+
+  float operator()(cluster_t const& cluster) const {
+    auto costgraph = costgraph_t::make(twolayer);
+    return costgraph(cluster);
+  }
+
+private:
+  cluster_t const cluster;
+  twolayergraph_t const twolayer;
+};
