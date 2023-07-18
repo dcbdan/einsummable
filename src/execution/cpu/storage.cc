c4250f1a8cf279e0e7ca07dbd3fefd20cb27fe8c
gravarica <gravara.ra60.2019@uns.ac.rs>
2023-07-18 11:30:06 -0500

    added internal data structure for managing free blocks in storage

--- src/execution/cpu/storage.cc
+++ src/execution/cpu/storage.cc
@@ -2,56 +2,95 @@
 
 void storage_manager_t::open_file()
 {
-    file.open(file_name, std::ios::binary | std::ios::out | std::ios::in);
-    if (!file) throw std::runtime_error("Error in open_file method. Failed to open the file: " + file_name + ".");
+	file.open(file_name, std::ios::binary | std::ios::out | std::ios::in);
+	if (!file) throw std::runtime_error("Error in open_file method. Failed to open the file: " + file_name + ".");
 }
 
 void storage_manager_t::write_to_disk(const buffer_t& buffer, const memloc_t& src, const stoloc_t& dst)
 {
-    open_file();
-    file.seekp(0, std::ios::end);
-    uint64_t position = file.tellp();
+	open_file();
+	auto const& block = find_first_available(src.size);
+	uint64_t position;
+	if (block == blocks.end())
+	{
+		file.seekp(0, std::ios::end);
+		allocate_block(block, src.size);
+	} 
+	else
+	{
+		file.seekp(block->beg, std::ios::beg);
+	}
 
-    file.write((char*)buffer->raw() + src.offset, src.size);
-    if (file.fail()) throw std::runtime_error("Error in write_to_disk method. Evicting data to disk unsuccessful.");
-    tensors[dst.id] = position;
+	position = file.tellp();
 
-    file.close();
+	file.write((char*)buffer->raw() + src.offset, src.size);
+	if (file.fail()) throw std::runtime_error("Error in write_to_disk method. Evicting data to disk unsuccessful.");
+	tensors[dst.id] = position;
+	
+	file.close();
 }
 
 void storage_manager_t::read_from_disk(const buffer_t& buffer, const stoloc_t& src, const memloc_t& dst)
 {
-    open_file();
-    uint64_t position = tensors[src.id];
-    file.seekg(positiong, std::ios::beg);
+	open_file();
+	uint64_t position = tensors[src.id];
+	file.seekg(position, std::ios::beg);
 
-    file.read((char*)buffer->raw() + dst.offset, dst.size);
-    if (file.fail()) throw std::runtime_error("Error in read_from_disk. Loading data from disk unsuccessful.");
-    tensors.erase(src.id);
+	file.read((char*)buffer->raw() + dst.offset, dst.size);
+	if (file.fail()) throw std::runtime_error("Error in read_from_disk. Loading data from disk unsuccessful.");
+	tensors.erase(src.id);
+	create_free_space(position, dst.size);
 
-    file.close();
+	file.close();
 }
 
 void storage_manager_t::evict(const evict_t& op, const buffer_t& buffer)
 {
-    try 
-    {
-        write_to_disk(buffer, op.src, op.dst);
-    }
-    catch (std::runtime_error e)
-    {
-        DOUT(e.what());
-    }   
+	write_to_disk(buffer, op.src, op.dst); 
 }
 
 void storage_manager_t::load(const load_t& op, const buffer_t& buffer)
 {
-    try
-    {
-        read_from_disk(buffer, op.src, op.dst);
-    }
-    catch(std::runtime_error e)
-    {
-        DOUT(e.what());
-    }
+	read_from_disk(buffer, op.src, op.dst);
+}
+
+vector<block_t>::iterator storage_manager_t::find_first_available(uint64_t size)
+{
+	using return_t = std::tuple<iter_t, iter_t, uint64_t>>;
+
+	for(iter_t iter = blocks.begin(); iter != blocks.end(); ++iter)
+	{
+		iter_t ret  = iter;
+		if (size <= iter->size()) 
+		{
+			break;
+		}
+	}
+
+	return iter;
+}
+
+void storage_manager_t::create_free_space(uint64_t position, uint64_t size)
+{
+	for (auto iter = blocks.begin(); iter != blocks.end(); ++iter)
+	{
+		if (iter->end == position)
+		{
+			iter->end = position+size;
+			return;
+		}
+	}
+
+	block_t block = {position, position + size};
+	blocks.push_back(block);
+}
+
+void storage_manager_t::allocate_block(vector<block_t>::iterator block, uint64_t size)
+{
+	if (size == block->size()) {
+		blocks.erase(block);
+		return;
+	}
+
+	block->beg += size;
 }
--- src/execution/cpu/storage.h
+++ src/execution/cpu/storage.h
@@ -7,19 +7,36 @@
 
 struct storage_manager_t 
 {
-    std::unordered_map<int, uint64_t> tensors;
-    std::fstream file;
-    std::string file_name;
+	std::unordered_map<int, uint64_t> tensors;
+	std::fstream file;
+	std::string file_name;
 
-    // Method that encapsulates logic of writing to file inside of storage manager
-    void evict(const evict_t& op, const buffer_t& buffer);
-    // Method that encapsulates logic of reading from file inside of storage manager
-    void load(const load_t&op, const buffer_t& buffer);
+	// Method that encapsulates logic of writing to file inside of storage manager
+	void evict(const evict_t& op, const buffer_t& buffer);
+	// Method that encapsulates logic of reading from file inside of storage manager
+	void load(const load_t&op, const buffer_t& buffer);
 
-    storage_manager_t(const string filename): file_name(filename){}
+	storage_manager_t(const string filename): file_name(filename){}
 private: 
-    void write_to_disk(const buffer_t& buffer, const memloc_t& src, const stoloc_t& dst);
-    void read_from_disk(const buffer_t& buffer, const stoloc_t& src, const memloc_t& dst);
-    // Method for safe opening of a file 
-    void open_file();
+	void write_to_disk(const buffer_t& buffer, const memloc_t& src, const stoloc_t& dst);
+	void read_from_disk(const buffer_t& buffer, const stoloc_t& src, const memloc_t& dst);
+	// Method for safe opening of a file 
+	void open_file();
+
+	struct block_t {
+			uint64_t beg;
+			uint64_t end;
+
+			uint64_t size() const { return end - beg; }
+	};
+
+	std::vector<block_t> blocks;
+	using iter_t = std::vector<block_t>::iterator;
+	
+	vector<block_t>::iterator find_first_available(uint64_t size);
+
+	void create_free_space(uint64_t position, uint64_t offset);
+	void allocate_block(uint64_t position, uint64_t offset);
+
+	void seek(uint64_t position); 
 };
