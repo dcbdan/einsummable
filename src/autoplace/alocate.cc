797041d59d359a2e7c729efb9d9ce44af8329915
Daniel Bourgeois <dcb_dan@hotmail.com>
2024-09-18 18:33:29 +0000

    added alocate04

--- src/autoplace/alocate.cc
+++ src/autoplace/alocate.cc
@@ -581,21 +581,24 @@ struct join_agg_info_t {
   vector<int> agg_locs;
 };
 
-vector<vector<int>> all_permutations(int nval) {
+vector<vector<int>> all_permutations(int nval, int choose) {
   if(nval < 0) {
     throw std::runtime_error("nval must be positive");
+  }
+  if(choose > nval || choose <= 0) {
+    throw std::runtime_error("invalid choose");
   }
 
   if(nval == 1) {
     return { {0} };
   }
 
-  auto fix = [&](vector<int> const& xs) {
+  auto idx_to_val = [&](vector<int> const& xs) {
     vector<int> ret;
-    vector<char> taken(xs.size() + 1, 0);
+    vector<char> taken(nval, 0);
     for(int x: xs) {
       int y = 0;
-      for(; y != taken.size(); ++y) {
+      for(; y != nval; ++y) {
         if(x == 0 && !taken[y]) {
           taken[y] = 1;
           break;
@@ -606,27 +609,24 @@ vector<vector<int>> all_permutations(int nval) {
       }
       ret.push_back(y);
     }
-    for(int y = 0; y != taken.size(); ++y) {
-      if(!taken[y]) {
-        ret.push_back(y);
-        break;
-      }
-    }
     return ret;
   };
 
   vector<int> shape;
-  for(int i = nval; i >= 2; --i) {
-    shape.push_back(i);
+  for(int i = 0; i != choose; ++i) {
+    shape.push_back(nval-i);
   }
-  vector<int> idxs(nval-1, 0);
-
+  vector<int> idxs(choose, 0);
   vector<vector<int>> ret;
   do {
-    ret.push_back(fix(idxs));
+    ret.push_back(idx_to_val(idxs));
   } while(indexer_utils<int>::increment_idxs(shape, idxs));
 
   return ret;
+}
+
+vector<vector<int>> all_permutations(int nval) {
+  return all_permutations(nval, nval);
 }
 
 vector<vector<int>> 
@@ -757,22 +757,446 @@ vector<placement_t> alocate03(
       rw.locations(join_gid) = join_locs;
       rw.locations(form_gid) = form_locs;
     } else {
-      int nbid = parts[gid].num_parts();
-      for(int bid = 0; bid != nbid; ++bid) {
-        jid_t jid{ gid, bid };
-        int best_loc = 0;
-        uint64_t best_cost = rw.compute_at(jid, 0);
-        for(int loc = 1; loc != nlocs; ++loc) {
-          uint64_t cost = rw.compute_at(jid, loc);
-          if(cost < best_cost) {
-            best_loc = loc;
-            best_cost = cost;
-          }
+      int const& join_gid = gid;
+      int n_join_bid = parts[join_gid].num_parts();
+      optional<tuple<uint64_t, vector<int>>> best;
+      vector<vector<int>> join_loc_options = all_permutations(nlocs, n_join_bid);
+      for(vector<int> const& join_locs: join_loc_options) {
+        uint64_t total_cost = 0;
+        for(int bid = 0; bid != n_join_bid; ++bid) {
+          jid_t jid{ join_gid, bid };
+          int const& loc = join_locs[bid];
+          total_cost += rw.compute_at(jid, loc);
+          rw.get_loc(jid) = loc;
         }
-        rw.get_loc(jid) = best_loc;
+
+        if(!best || total_cost < std::get<0>(best.value())) {
+          best = tuple<uint64_t, vector<int>> {total_cost, join_locs };
+        }
+
+        rw.locations(join_gid) = vector<int>(n_join_bid, -1);
       }
+
+      auto const& [_, join_locs] = best.value();
+      rw.locations(join_gid) = join_locs;
     }
   }
 
   return rw.get_placements();
 }
+
+topology_t::topology_t()
+  : width_factor(1)
+{}
+
+void topology_t::insert_link(int src, int dst, int width) {
+  if(is_costing()) {
+    throw std::runtime_error("cannot add links while costing; call reset");
+  }
+
+  {
+    bool has_path = bool(get_path(src, dst));
+    if(has_path) {
+      throw std::runtime_error("cannot insert direct link, there is a path");
+    }
+  }
+
+  auto maybe = get_link(src, dst);
+  if(maybe) {
+    link_t& l = links[maybe.value()];
+    l.width += width;
+
+    update_width_factor(l.width);
+  } else {
+    links.push_back(link_t {
+      .src = src,
+      .dst = dst,
+      .width = width
+    });
+
+    update_width_factor(width);
+  }
+}
+
+void topology_t::insert_path(int src, int dummy, int dst) {
+  if(is_costing()) {
+    throw std::runtime_error("cannot add paths while costing; call reset");
+  }
+
+  {
+    bool has_link = bool(get_link(src, dst));
+    if(has_link) {
+      throw std::runtime_error("cannot insert path, there is a direct link");
+    }
+  }
+
+  {
+    bool has_path = bool(get_path(src, dst));
+    if(has_path) {
+      throw std::runtime_error("there is already a path");
+    }
+  }
+
+  paths.push_back(path_t { .src = src, .mid = dummy, .dst = dst });
+}
+
+void topology_t::add_move(int src, int dst, uint64_t bytes) {
+  for(int which_link: get_links_for_move(src, dst)) {
+    cost_per_link[which_link] += cost_at_link(which_link, bytes);
+  }
+}
+
+uint64_t topology_t::cost_moves(
+  vector<tuple<int, int, uint64_t>> const& moves) const 
+{
+  // just make a copy and do the moves
+  topology_t other = *this;
+  for(auto const& [src,dst,size]: moves) {
+    other.add_move(src, dst, size);
+  }
+  return other.cost();
+}
+
+uint64_t topology_t::cost() const {
+  uint64_t ret = 0;
+  for(uint64_t const& c: cost_per_link) {
+    ret = std::max(ret, c);
+  }
+  return ret;
+}
+
+void topology_t::reset() {
+  for(uint64_t& c: cost_per_link) {
+    c = 0;
+  }
+}
+
+optional<int> topology_t::get_link(int src, int dst) const {
+  auto iter = src_to_link.find(src);
+  if(iter == src_to_link.end()) {
+    return std::nullopt;
+  }
+  return optional<int>(iter->second);
+}
+
+optional<int> topology_t::get_path(int src, int dst) const {
+  for(auto const& [_src, ret, _dst]: paths) {
+    if(src == _src && dst == _dst) {
+      return optional<int>(ret);
+    }
+  }
+  return std::nullopt;
+}
+
+int topology_t::get_link_width(int src, int dst) const {
+  return links[get_link(src, dst).value()].width;
+}
+int topology_t::get_link_width(int which_link) const {
+  return links[which_link].width;
+}
+
+vector<int> topology_t::get_links_for_move(int src, int dst) const {
+  auto maybe_which_link = get_link(src, dst);
+  if(maybe_which_link) {
+    return vector<int>{ maybe_which_link.value() };
+  } else {
+    int mid = get_path(src, dst).value();
+    return vector<int>{
+      get_link(src, mid).value(),
+      get_link(mid, dst).value()
+    };
+  }
+}
+
+uint64_t topology_t::cost_at_link(int which_link, uint64_t bytes) const {
+  int w = get_link_width(which_link);
+  if(width_factor % w != 0) {
+    throw std::runtime_error("should not occur: invalid width factor");
+  }
+
+  uint64_t multiplier = width_factor / w;
+  return bytes*multiplier;
+}
+
+bool topology_t::is_costing() const {
+  for(auto const& c: cost_per_link) {
+    if(c > 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void topology_t::update_width_factor(int _) {
+  // just set the width factor from all the widths
+
+  int w = 1;
+  for(auto const& l: links) {
+    w = std::max(w, l.width);
+  }
+
+  for(auto const& l: links) {
+    if(w % l.width != 0) {
+      w *= l.width;
+    }
+  }
+
+  width_factor = w;
+}
+
+struct _alocate04_rw_t {
+  _alocate04_rw_t(
+    graph_t const& graph,
+    vector<partition_t> const& parts,
+    int nlocs);
+
+  vector<int> const&
+  locations(int gid) const {
+    return _rw.ginfos[gid].locations;
+  }
+  vector<int>&
+  locations(int gid) {
+    return _rw.ginfos[gid].locations;
+  }
+
+  int& get_loc(jid_t const& jid) {
+    return _rw.get_loc(jid);
+  }
+
+  int const& get_loc(jid_t const& jid) const {
+    return _rw.get_loc(jid);
+  }
+
+  vector<placement_t> get_placements() const {
+    return _rw.get_placements();
+  }
+
+  vector<tuple<int, int, uint64_t>> get_moves(
+   jid_t const& jid,
+   int this_loc) const;
+
+  void compute_at(topology_t& topology, jid_t const& jid, int loc) const;
+
+  // same as compute at, but greedily choose the best
+  // location
+  int compute_at_best_loc(topology_t& topology, jid_t const& jid) const;
+
+private:
+  relationwise_t _rw;
+  int nlocs;
+};
+
+_alocate04_rw_t::_alocate04_rw_t(
+  graph_t const& graph,
+  vector<partition_t> const& parts,
+  int nlocs)
+  : _rw(graph, parts), nlocs(nlocs)
+{}
+
+vector<tuple<int, int, uint64_t>> _alocate04_rw_t::get_moves(
+ jid_t const& jid,
+ int this_loc) const
+{
+  vector<tuple<int, int, uint64_t>> ret;
+  for(rid_t const& rid: _rw.get_join(jid).deps) {
+    // this guy will need to be at the join location
+  
+    set<int> refi_usage_locs = _rw.get_refi_usage_locs(rid);
+    if(refi_usage_locs.count(this_loc) != 0) {
+      // already here, nothing to do
+      continue;
+    }
+
+    vector<int> const& join_locs = _rw.ginfos[rid.gid].locations;
+    auto const& refi = _rw.get_refi(rid);
+    for(auto const& unit: refi.units) {
+      set<int> src_locs;
+      for(int const& join_bid: unit.deps) {
+        int const& join_loc = join_locs[join_bid];
+        if(join_loc != this_loc) {
+          src_locs.insert(join_loc);
+        }
+      }
+
+      for(int const& src: src_locs) {
+        ret.push_back({src, this_loc, unit.size});
+      }
+    }
+  }
+  return ret;
+}
+
+void _alocate04_rw_t::compute_at(
+  topology_t& topology, jid_t const& jid, int this_loc) const
+{
+  for(auto const& [src, dst, size]: get_moves(jid, this_loc)) {
+    topology.add_move(src, dst, size);
+  }
+}
+
+struct _compute_at_best_loc_t {
+  int loc;
+  vector<tuple<int, int, uint64_t>> moves;
+  uint64_t cost;
+};
+int _alocate04_rw_t::compute_at_best_loc(
+  topology_t& topology, jid_t const& jid) const
+{
+  optional<_compute_at_best_loc_t> best;
+  for(int loc = 0; loc != nlocs; ++loc) {
+    auto moves = get_moves(jid, loc);
+    uint64_t cost = topology.cost_moves(moves);
+
+    if(!bool(best) || cost < best.value().cost) {
+      best = _compute_at_best_loc_t {
+        .loc = loc,
+        .moves = moves,
+        .cost = cost
+      };
+    }
+  }
+
+  for(auto const& [src, dst, size]: best.value().moves) {
+    topology.add_move(src, dst, size);
+  }
+
+  return best.value().loc;
+};
+
+vector<placement_t> alocate04(
+  graph_t const& graph,
+  vector<partition_t> const& parts,
+  int nlocs,
+  topology_t topology)
+{
+  _alocate04_rw_t rw(graph, parts, nlocs);
+
+  auto is_full_plan = [&](int gid) {
+    auto const& op = graph.nodes[gid].op;
+    if(op.is_einsummable()) {
+      auto const& e = op.get_einsummable();
+      if(e.is_contraction()) {
+        return true;
+      }
+      if(e.has_aggregation() && parts[gid].num_parts() == nlocs) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  for(int const& gid: graph.get_order()) {
+    auto const& node = graph.nodes[gid];
+
+    // TODO: how to deal with inputs?
+    if(node.op.is_input()) {
+      int nbid = parts[gid].num_parts();
+      for(int bid = 0; bid != nbid; ++bid) {
+        jid_t jid{ gid, bid };
+        rw.get_loc(jid) = bid % nlocs;
+      }
+
+      continue;
+    }
+
+    if(node.op.is_formation()) {
+      if(node.inns.size() != 1) {
+        throw std::runtime_error("a formation must have one input");
+      }
+      int inn_gid = node.inns[0];
+      if(is_full_plan(inn_gid)) {
+        // when doing the input node, this node was already set
+        continue;
+      }
+    }
+
+    if(is_full_plan(gid))
+    {
+      if(node.outs.size() != 1) {
+        throw std::runtime_error("must have one out");
+      }
+
+      int const& join_gid = gid;
+      int        form_gid = *node.outs.begin();
+
+      if(!graph.nodes[form_gid].op.is_formation()) {
+        throw std::runtime_error("must have one formation out");
+      }
+
+      int n_join_bid = parts[join_gid].num_parts();
+      int n_form_bid = parts[form_gid].num_parts();
+      if(n_join_bid > nlocs) {
+        throw std::runtime_error("expect join <= nlocs");
+      }
+      optional<join_agg_info_t> best;
+      for(vector<int> const& join_locs: all_permutations(nlocs, n_join_bid)) {
+        topology.reset();
+
+        // 1. set all the joins 
+        for(int bid = 0; bid != n_join_bid; ++bid) {
+          jid_t jid{ join_gid, bid };
+          int const& loc = join_locs[bid];
+          rw.compute_at(topology, jid, loc);
+          rw.get_loc(jid) = loc;
+        }
+
+        uint64_t join_cost = topology.cost();
+        topology.reset();
+
+        // 2. set all the aggs 
+        for(int bid = 0; bid != n_form_bid; ++bid) {
+          jid_t jid{ form_gid, bid };
+          int best_loc = rw.compute_at_best_loc(topology, jid);
+          rw.get_loc(jid) = best_loc;
+        }
+
+        uint64_t agg_cost = topology.cost();
+        uint64_t total_cost = join_cost + agg_cost;
+
+        if(!best || total_cost < best.value().cost) {
+          best = join_agg_info_t { 
+            .cost = total_cost, 
+            .join_locs = rw.locations(join_gid),
+            .agg_locs  = rw.locations(form_gid)
+          };
+        }
+
+        rw.locations(join_gid) = vector<int>(n_join_bid, -1);
+        rw.locations(form_gid) = vector<int>(n_form_bid, -1);
+      }
+
+      auto const& [_, join_locs, form_locs] = best.value();
+      rw.locations(join_gid) = join_locs;
+      rw.locations(form_gid) = form_locs;
+    } else {
+      int join_gid = gid;
+      int n_join_bid = parts[join_gid].num_parts();
+      optional<tuple<uint64_t, vector<int>>> best;
+      vector<vector<int>> join_loc_options = all_permutations(nlocs, n_join_bid);
+      for(vector<int> const& join_locs: join_loc_options) {
+        topology.reset();
+        for(int bid = 0; bid != n_join_bid; ++bid) {
+          jid_t jid{ join_gid, bid };
+          int const& loc = join_locs[bid];
+          rw.compute_at(topology, jid, loc);
+          rw.get_loc(jid) = loc;
+        }
+
+        uint64_t total_cost = topology.cost();
+        if(!best || total_cost < std::get<0>(best.value())) {
+          best = tuple<uint64_t, vector<int>> {total_cost, join_locs};
+        }
+
+        rw.locations(join_gid) = vector<int>(n_join_bid, -1);
+      }
+
+      auto const& [_, join_locs] = best.value();
+      rw.locations(join_gid) = join_locs;
+    }
+  }
+
+  return rw.get_placements();
+}
+
+
+
